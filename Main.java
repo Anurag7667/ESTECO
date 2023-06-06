3b226058b69d2ebd1d4d751f1cf51dae83a9b673
Anurag7667 <anurag.gargote@aissmsioit.org>
2023-06-07 01:01:47 +0530

    Third update.

--- Main.java
+++ Main.java
@@ -7,19 +7,46 @@ public class Main {
         System.out.print("Enter the numbers separated by comma: ");
         String input = scanner.nextLine();
 
-        String result = add(input.split(","));
+        String result = add(input);
         System.out.println("Sum: " + result);
+
+        scanner.close(); // Close the scanner
+
     }
 
-    public static String add(String... numbers) {
-        if (numbers.length == 0) {
+    public static String add(String input) {
+        if (input.isEmpty()) {
             return "0";
         }
 
-        double sum = 0.0;
+        if (input.endsWith(",") || input.endsWith("\n")) {
+            return "Number expected but EOF found.";
+        }
 
-        for (String number : numbers) {
-            sum += Double.parseDouble(number);
+        String[] numbers = input.split("[,\n]");
+
+        double sum = 0.0;
+        int invalidPosition = -1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            String number = numbers[i].trim();
+
+            if (number.isEmpty()) {
+                invalidPosition = i;
+                break;
+            }
+
+            try {
+                sum += Double.parseDouble(number);
+            } catch (NumberFormatException e) {
+                invalidPosition = i;
+                break;
+            }
+        }
+
+        if (invalidPosition != -1) {
+            return "Number expected but '" + numbers[invalidPosition] + "' found at position "
+                    + input.indexOf(numbers[invalidPosition]) + ".";
         }
 
         return Double.toString(sum);
